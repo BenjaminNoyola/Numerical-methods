b442bf4fed838519b07dcf877a75ad1420529309
Benjamin Noyola <benjamin.noyola@intelmas.com>
2020-08-12 13:21:49 -0500

    Actualización a python 3.8

--- 2_Int_Real_OperacA.py
+++ 2_Int_Real_OperacA.py
@@ -1,6 +1,3 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
 # Autor: Benjamín Salomón Noyola García
 # Tema2: Tipos de numeros: enteros, reales y operaciones aritméticas.
 
@@ -10,29 +7,29 @@ c=2
 d=3.5
 
 #suma
-print a+c
+print(a+c)
 
 #resta
-print b-a
-print "a-c:  ",a-c
+print(b-a)
+print("a-c:  ",a-c)
 
 #multiplicación
-print d*c
+print(d*c)
 
 #División
-print b/c
-print "a/c   ",a/c
-print "4/3",4/3,"   4.0/3:  ",4.0/3
+print(b/c)
+print("a/c   ",a/c)
+print("4/3",4/3,"   4.0/3:  ",4.0/3)
 
 #Potencia
-print a**c
+print(a**c)
 
 #from numpy import *
 import numpy as np 
 
 #Raiz
-print np.sqrt(9.317)
-print 9.317**0.5
+print(np.sqrt(9.317))
+print(9.317**0.5)
 
 # Módulo
-print 5%2
+print(5%2)
